c83d4fc9e64f5d353a3d3e07a4724adf98814653
David <davidblkx@gmail.com>
2018-03-26 21:49:33 +0100

    initial

---
+++ Plugin.cs
@@ -0,0 +1,26 @@
+﻿using Subble.Core;
+using Subble.Core.Plugin;
+using System;
+using System.Collections.Generic;
+
+namespace BasicLogger
+{
+    public class Plugin : ISubblePlugin
+    {
+        public IPluginInfo Info
+            => new PluginInfo();
+
+        public SemVersion Version
+            => new SemVersion(0, 0, 1);
+
+        public long LoadPriority => 3;
+
+        public IEnumerable<Dependency> Dependencies
+            => new List<Dependency>();
+
+        public bool Initialize(ISubbleHost host)
+        {
+            return true;
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,19 @@
+﻿using Subble.Core.Plugin;
+
+namespace BasicLogger
+{
+    public class PluginInfo : IPluginInfo
+    {
+        public string GUID => "e34f2495-8f6f-46fd-a90d-8a5606ea5a71";
+
+        public string Name => "BasicLogger";
+
+        public string Creator => "DavidPires";
+
+        public string Repository => "https://github.com/Davidblkx/Subble";
+
+        public string Support => "https://github.com/Davidblkx/Subble/issues";
+
+        public string Licence => "MIT";
+    }
+}
