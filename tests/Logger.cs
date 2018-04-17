701752d8e2ea441eea81a74dfa88352ce8dd6d87
David Pires <davidblkx@gmail.com>
2018-04-17 11:57:34 +0100

    Added tests and change folders to match subble

---
+++ tests/Logger.cs
@@ -0,0 +1,15 @@
+#if DEBUG
+using Xunit;
+
+namespace BasicLogger.Tests 
+{
+    public class Logger 
+    {
+        [Fact]
+        public void Logger_main()
+        {
+            Assert.True(true);
+        }
+    }
+}
+#endif
